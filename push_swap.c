7780fa553f954b66498f848455777a71142b939e
Florian Muller <flmuller@c3r3p6.42perpignan.fr>
2023-12-02 16:03:16 +0100

    add pile init function

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: flmuller <flmuller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 12:48:23 by flmuller          #+#    #+#             */
-/*   Updated: 2023/11/22 17:24:41 by flmuller         ###   ########.fr       */
+/*   Updated: 2023/11/30 13:27:42 by flmuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,4 +62,105 @@ void	rotate(t_pile **node, int nbrot)
 		*node = (*node)->previous;
 		i++;
 	}
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	sign = 1;
+	res = 0;
+	i = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == ' ')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		res = res * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
+
+t_pile	*init_node(int value, t_pile *previous, t_pile *next)
+{
+	t_pile	*node;
+
+	node = malloc(sizeof(t_pile));
+	if (!node)
+		return(NULL);
+	node->value = value;
+	if (next)
+		node->next = next;
+	if (previous)
+		node->previous = previous;
+	return (node);
+}
+
+
+/*
+	Function: init_pile
+	-------------------
+	Init an list of t_pile struct with provided numbers.
+	return pointer on the first element of the list.
+	return Null in case of Error.
+ */
+t_pile	**init_pile(char **numbers, unsigned int nbelem)
+{
+	int	i;
+	t_pile	*node;
+	t_pile	*previous;
+	t_pile	*next;
+	t_pile	*first;
+
+	i = 0;
+	node = malloc(sizeof(t_pile));
+	if (!node)
+		return(NULL);
+	first = node;
+	while (!node->previous)
+	{
+		if (!node->value)
+		{
+			node->value = ft_atoi(numbers[i]);
+			i++;
+		}
+		if (i < nbelem)
+		{
+		next = malloc(sizeof(t_pile));
+		if (!next)
+			return(NULL);
+		}
+		else
+			next = first;
+		node->next = next;
+		if (previous)
+			node->previous = previous;
+		previous = node;
+		node = next;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_pile	*pile_a;
+	t_pile	*pile_b;
+
+	if (argc > 1)
+	{
+		
+	}
+	else
+	{
+		write(1, "Error", 5);
+	}
+	write(1, '\n', 1);
+	return(0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,12 +6,14 @@
 /*   By: flmuller <flmuller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 10:08:15 by flmuller          #+#    #+#             */
-/*   Updated: 2023/11/22 10:09:52 by flmuller         ###   ########.fr       */
+/*   Updated: 2023/11/30 13:13:21 by flmuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+# include <unistd.h>
 
 typedef struct s_pile
 {
@@ -19,5 +21,9 @@ typedef struct s_pile
 	struct pile_list	*next;
 	struct pile_list	*previous;
 }	t_pile;
+
+void	swap(t_pile *node);
+void	push(t_pile **ntpush, t_pile **pushpile);
+void	rotate(t_pile **node, int nbrot);
 
 #endif
